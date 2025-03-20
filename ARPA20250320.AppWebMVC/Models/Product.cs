ffa233f3ade0a914d137cb286acf2df8f93c6935
andrearocioperez <rocioarriola1204@outlook.com>
2025-03-20 09:13:06 -0600

    Coloque las validaciones en los modelos.

--- ARPA20250320.AppWebMVC/Models/Product.cs
+++ ARPA20250320.AppWebMVC/Models/Product.cs
@@ -1,23 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ARPA20250320.AppWebMVC.Models;
 
 public partial class Product
 {
+    [Display(Name = "IdProducto")]
     public int ProductId { get; set; }
-
+    [Display(Name = "Nombre")]
+    [Required(ErrorMessage = "El nombre es obligatorio.")]
     public string ProductName { get; set; } = null!;
-
+    [Display(Name = "Descripcion")]
     public string? Description { get; set; }
-
+    [Display(Name = "Precio")]
+    [Required(ErrorMessage = "El precio es obligatorio.")]
     public decimal Price { get; set; }
-
+    [Display(Name = "Bodega")]
     public int? WarehouseId { get; set; }
-
+    [Display(Name = "Marca")]
     public int? BrandId { get; set; }
-
+    [Display(Name = "Marca")]
     public virtual Brand? Brand { get; set; }
-
+    [Display(Name = "Bodega")]
     public virtual Warehouse? Warehouse { get; set; }
 }
