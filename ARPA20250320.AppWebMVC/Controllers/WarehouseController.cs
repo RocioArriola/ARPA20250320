719c2d74dc52cf97d9d4dd808dcbe732fe742505
andrearocioperez <rocioarriola1204@outlook.com>
2025-03-23 22:35:08 -0600

    se realizo la paginacion del controlador warehouse y se edito las vistas index de marca y producto

--- ARPA20250320.AppWebMVC/Controllers/WarehouseController.cs
+++ ARPA20250320.AppWebMVC/Controllers/WarehouseController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using ARPA20250320.AppWebMVC.Models;
 using Microsoft.AspNetCore.Authorization;
+using System.Drawing.Drawing2D;
 
 namespace ARPA20250320.AppWebMVC.Controllers
 {
@@ -21,9 +22,18 @@ namespace ARPA20250320.AppWebMVC.Controllers
         }
 
         // GET: Warehouse
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(Warehouse warehouse, int topRegistro = 10)
         {
-            return View(await _context.Warehouses.ToListAsync());
+            var query = _context.Warehouses.AsQueryable();
+            if (!string.IsNullOrWhiteSpace(warehouse.WarehouseName))
+                query = query.Where(s => s.WarehouseName.Contains(warehouse.WarehouseName));
+            if (!string.IsNullOrWhiteSpace(warehouse.Notes))
+                query = query.Where(s => s.Notes.Contains(warehouse.Notes));
+            if (topRegistro > 0)
+                query = query.Take(topRegistro);
+
+
+            return View(await query.ToListAsync());
         }
 
         // GET: Warehouse/Details/5
